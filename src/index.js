898e9c80b4089e90ed6687c3c917d153631c528c
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2024-10-24 15:22:11 +1100

    Init

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// js
+import { greeting } from "./greeting.js"
+
+// css
+import "./styles.css";
+
+console.log(greeting);
---
+++ webpack.prod.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+	mode: "production",
+	entry: "./src/index.js",
+	output: {
+		filename: "main.js",
+		path: path.resolve(__dirname, "dist"),
+		clean: true,
+	},
+	// next 2 are for auto builds on save
+	devtool: "eval-source-map",
+	// reloads html file as npm plugin 'webpack-dev-server' auto reloads js files
+	devServer: {
+		watchFiles: ["./src/template.html"],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./src/template.html",
+		})
+	],
+	module: {
+		rules: [
+			{
+				// load css files
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader"],
+			},
+			{
+				// images loaded in a html file e.g.
+				// <img src="./imgPath">
+				test: /\.html$/i,
+				loader: "html-loader",
+			},
+			{
+				// images sourced in js
+				// need to import them into a variable e.g.
+				// import img from "./imgPath"
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: "asset/resource",
+			},
+		],
+	},
+};
