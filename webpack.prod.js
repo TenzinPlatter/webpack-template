b3bd0125df006ca08b3e7ab22643d4e726132d9c
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2024-10-24 15:42:04 +1100

    Added webpack common file

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,45 +1,6 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
 
-module.exports = {
-	mode: "production",
-	entry: "./src/index.js",
-	output: {
-		filename: "main.js",
-		path: path.resolve(__dirname, "dist"),
-		clean: true,
-	},
-	// next 2 are for auto builds on save
-	devtool: "eval-source-map",
-	// reloads html file as npm plugin 'webpack-dev-server' auto reloads js files
-	devServer: {
-		watchFiles: ["./src/template.html"],
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: "./src/template.html",
-		})
-	],
-	module: {
-		rules: [
-			{
-				// load css files
-				test: /\.css$/i,
-				use: ["style-loader", "css-loader"],
-			},
-			{
-				// images loaded in a html file e.g.
-				// <img src="./imgPath">
-				test: /\.html$/i,
-				loader: "html-loader",
-			},
-			{
-				// images sourced in js
-				// need to import them into a variable e.g.
-				// import img from "./imgPath"
-				test: /\.(png|svg|jpg|jpeg|gif)$/i,
-				type: "asset/resource",
-			},
-		],
-	},
-};
+module.exports = merge(common, {
+	mode: "production"
+})
